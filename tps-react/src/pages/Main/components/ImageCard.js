2c796ef4b1db3edf7b8ecb7ce0a9eeba97062f68
yanglihao347 <yanglihao347@gmail.com>
2021-06-08 00:54:11 +0800

    修改图片样式

--- tps-react/src/pages/Main/components/ImageCard.js
+++ tps-react/src/pages/Main/components/ImageCard.js
@@ -6,14 +6,41 @@ import styles from '../index.module.css';
 class ImageCard extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      type: 0,
+    };
   }
+
+  componentDidMount() {
+    const { item } = this.props;
+    if (item.image_width >= item.image_height) {
+      this.setState({
+        type: 1,
+      });
+    }
+  }
+
+  renderSize = () => {
+    const { item } = this.props;
+    let cname = '';
+    if (item.image_width <= 100 && item.image_height <= 100) {
+      cname = '';
+    } else if (item.image_width <= item.image_height) {
+      cname = styles['height-image'];
+    } else {
+      cname = styles['width-image'];
+    }
+    return cname;
+  };
+
   render() {
     const { item } = this.props;
+    const { type } = this.state;
+
     return (
-      <CopyToClipboard text={item.img_url} className={styles['clipboard']}>
+      <CopyToClipboard text={item.img_url} className={styles['img-container']}>
         <div className={styles['img-container']}>
-          <img src={item.img_url} />
+          <img className={this.renderSize()} src={item.img_url} />
         </div>
       </CopyToClipboard>
     );
